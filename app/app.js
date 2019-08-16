5c750ca5b9ec7d681e24a80b2a639eb55185b4d8
Christopher Tjong <ctjong@ctjong-mn1.linkedin.biz>
2019-08-16 13:03:50 -0700

    uncomment

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-// import Controller from '@ember/controller';
-// import { alias } from '@ember/object/computed';
+import Controller from '@ember/controller';
+import { alias } from '@ember/object/computed';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
@@ -15,10 +15,10 @@ const App = Application.extend({
 
 loadInitializers(App, config.modulePrefix);
 
-// // Maintain compatibility w/ ember 1.13 -> 3.7.1 and beyond
-// // https://www.emberjs.com/deprecations/v2.x/#toc_controller-content-alias
-// Controller.reopen({
-//   content: alias('model')
-// });
+// Maintain compatibility w/ ember 1.13 -> 3.7.1 and beyond
+// https://www.emberjs.com/deprecations/v2.x/#toc_controller-content-alias
+Controller.reopen({
+  content: alias('model')
+});
 
 export default App;
