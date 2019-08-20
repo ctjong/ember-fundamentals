71ea3f9f6907fbe15e5bfcd3608b56201a9e4afc
Christopher Tjong <ctjong@ctjong-mn1.linkedin.biz>
2019-08-19 18:03:38 -0700

    fixed ex7 test

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,16 +3,18 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model()
   {
-    return fetch("https://api.mike.works/api/v1/courses").then(async response =>
-    {
-      const data = await response.json()
-      return data.data.map(item => (
-        {
-          title: item.attributes.title,
-          summary: item.attributes.summary,
-          'image-info': item.attributes['image-info']
-        }
-      ));
-    });
+    return fetch("https://api.mike.works/api/v1/courses")
+      .then(response => response.json())
+      .then(async json =>
+      {
+        return json.data.map(item => (
+          {
+            title: item.attributes.title,
+            summary: item.attributes.summary,
+            slug: item.attributes.slug,
+            'image-info': item.attributes['image-info']
+          }
+        ));
+      });
   }
 });
--- app/routes/course.js
+++ app/routes/course.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.modelFor('application').filter(c => c.title === params.id)[0]
+    return this.modelFor('application').filter(c => c.slug === params.id)[0]
   }
 });
